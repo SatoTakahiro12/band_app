0ead9d2263dcfd57e821ba1bc0737474c72ad9e7
SatoTakahiro12 <1921319t@hgu.jp>
2023-08-26 02:08:02 +0900

    Fix:カテゴリー重複、レイアウト、プロフィールの編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -26,7 +26,7 @@
         @endif
     </div>
 
-    <div class="mx-auto px-6">
+    <div class="max-w-7xl mx-auto px-6">
         <div class="mt-4 p-8 bg-white w-full rounded-2xl">
             <h1 class="p-4 text-lg font-semibold">
                 {{$post->title}}
@@ -112,7 +112,7 @@
         </span>
     </div>
     <!---コメント機能--->
-    <div class="mx-auto px-6">
+    <div class="max-w-7xl mx-auto px-6">
         <h1 class = "text-xl text-gray-800 ">---コメント一覧（新しい順）---</h1>
         @foreach($comments as $comment)
             @if($post->id == $comment->post_id)
