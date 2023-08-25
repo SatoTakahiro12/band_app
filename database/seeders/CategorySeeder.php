99ff18bd75cff83e11d541136a704d22e7b11c5d
SatoTakahiro12 <1921319t@hgu.jp>
2023-08-26 03:01:30 +0900

    Add：CategorySeederのid

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,18 +16,21 @@ class CategorySeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
+            'id'=>'1',
             'name'=>'語ります！',
             'created_at'=>new DateTime(),
             'updated_at'=>new DateTime(),
             ]);
         
          DB::table('categories')->insert([
+             'id'=>'2',
             'name'=>'ライブ同行者募集！',
             'created_at'=>new DateTime(),
             'updated_at'=>new DateTime(),
             ]);
             
          DB::table('categories')->insert([
+            'id'=>'3',
             'name'=>'セトリ',
             'created_at'=>new DateTime(),
             'updated_at'=>new DateTime(),
